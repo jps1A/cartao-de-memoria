263d2a425e015def8307380c4375f5f47ea04e58
João Pedro Silva <167330575+jps1A@users.noreply.github.com>
2025-06-23 11:23:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Matamática',
+    'Quantos é 1+1?',
+    'A resposta para 1+1 é 2'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Eduação fisica',
+    'Quantos ossos tem o corpo humano?',
+    'O corpo humano tem 206'
+)
