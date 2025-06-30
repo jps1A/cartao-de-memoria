8c8f7b9a13d00d6fb67a9460093172ff22fe9b89
João Pedro Silva <167330575+jps1A@users.noreply.github.com>
2025-06-30 11:14:15 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,23 @@
 criaCartao(
-    'Matamática',
+    'Matemática',
     'Quantos é 1+1?',
     'A resposta para 1+1 é 2'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital do Brasil?',
-    'A capital do Brasil é Brasília'
+    'Qual a capitao do Brasil?',
+    'A capital do Brasil é brasília'
 )
 
 criaCartao(
-    'Eduação fisica',
+    'Ciências',
+    'O que é H2o?',
+    'O H2o é a água'
+)
+
+criaCartao(
+    'Educação fisica',
     'Quantos ossos tem o corpo humano?',
-    'O corpo humano tem 206'
+    'O corpo humano tem 206 ossos'
 )
